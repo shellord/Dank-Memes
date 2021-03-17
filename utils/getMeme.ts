bd7e904ef16a6eb5a06977b2099f31dde73cfbbf
saheen shoukath <saheenshoukath@gmail.com>
2021-03-17 11:58:18 +0530

    idk

--- utils/getMeme.ts
+++ utils/getMeme.ts
@@ -2,7 +2,7 @@ import {API_URL} from '../config/config'
 
 const getMeme = async (subreddit:string) => {
     // let res = await fetch(`${API_URL}/${subreddit}/20`)
-    let res = await fetch(`${API_URL}/20`)
+    let res = await fetch(`${API_URL}/10`)
     let data = await res.json()
     return data.memes
 }
